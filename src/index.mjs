1d3803c050f19bc0b1781f82efbcde2b3853a291
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2023-10-25 21:41:10 +0530

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+`;
+
+const num=6
+let y = [0,1,1,2,3,5,8]
+
+
+function fibonacci(n){
+  
+}
+
+// console.log(fibonacci(5))
+
+
+
+
+
